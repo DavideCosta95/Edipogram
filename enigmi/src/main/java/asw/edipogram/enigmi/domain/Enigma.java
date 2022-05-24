28b546b294791a62e9004dce5f990b956005e708
Gren <dashish95@gmail.com>
2022-05-24 22:50:58 +0200

    feat(enigmi): add Enigma entity implementation

--- enigmi/src/main/java/asw/edipogram/enigmi/domain/Enigma.java
+++ enigmi/src/main/java/asw/edipogram/enigmi/domain/Enigma.java
@@ -2,38 +2,64 @@ package asw.edipogram.enigmi.domain;
 
 import javax.persistence.*; 
 
-import lombok.*; 
+import lombok.*;
+import org.hibernate.annotations.Type;
 
 /* Enigma, in formato completo. */ 
-@Entity 
-@Data @NoArgsConstructor
+@Entity(name = "enigmi")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Enigma implements Comparable<Enigma> {
 
-	@Id 
-	@GeneratedValue
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
-	private Long id; 
-	private String autore; 
-	private String tipo; 
-	private String tipoSpecifico; 
-	private String titolo; 
-	private String[] testo; 
-	private String[] soluzione; 
-	
-	public Enigma(String autore, String tipo, String tipoSpecifico, String titolo, String[] testo, String[] soluzione) {
-		this(); 
-		this.autore = autore; 
-		this.tipo = tipo; 
-		this.tipoSpecifico = tipoSpecifico; 
-		this.titolo = titolo; 
-		this.testo = testo; 
-		this.soluzione = soluzione; 
+	@Column(name="id", nullable = false)
+	@NonNull
+	private Long id;
+
+	@Column(name="autore", nullable = false)
+	@NonNull
+	private String autore;
+
+	@Column(name="tipo", nullable = false)
+	@NonNull
+	private String tipo;
+
+	@Column(name="tipo_specifico", nullable = false)
+	@NonNull
+	private String tipoSpecifico;
+
+	@Column(name="titolo", nullable = false)
+	@NonNull
+	private String titolo;
+
+	@Column(name = "testo", nullable = false, columnDefinition = "text[]")
+	@NonNull
+	@Type(type = "asw.edipogram.enigmi.domain.StringArrayType")
+	private String[] testo;
+
+	@Column(name = "soluzione", nullable = false, columnDefinition = "text[]")
+	@NonNull
+	@Type(type = "asw.edipogram.enigmi.domain.StringArrayType")
+	private String[] soluzione;
+
+	public Enigma(@NonNull String autore, @NonNull String tipo, @NonNull String tipoSpecifico, @NonNull String titolo, @NonNull String[] testo, @NonNull String[] soluzione) {
+		this();
+		this.autore = autore;
+		this.tipo = tipo;
+		this.tipoSpecifico = tipoSpecifico;
+		this.titolo = titolo;
+		this.testo = testo;
+		this.soluzione = soluzione;
 	}
 
 	@Override
 	public int compareTo(Enigma other) {
 		return this.id.compareTo(other.id); 
 	}
-	
+
 }
