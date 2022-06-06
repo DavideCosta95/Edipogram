81670804d23cdb0ddc1e72e0cdbd329f15b32b21
Gren <dashish95@gmail.com>
2022-06-06 14:30:45 +0200

    refactor(enigmi): improve dependencies injection

--- enigmi/src/main/java/asw/edipogram/enigmi/domain/EnigmiService.java
+++ enigmi/src/main/java/asw/edipogram/enigmi/domain/EnigmiService.java
@@ -1,54 +1,46 @@
 package asw.edipogram.enigmi.domain;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.util.logging.Logger; 
-import java.util.*; 
-import java.util.stream.*; 
+import java.util.*;
 
 @Service
 public class EnigmiService {
 
+	private final EnigmiRepository enigmiRepository;
+
 	@Autowired
-	private EnigmiRepository enigmiRepository;
+	private EnigmiService(EnigmiRepository enigmiRepository) {
+		this.enigmiRepository = enigmiRepository;
+	}
 
  	public Enigma createEnigma(String autore, String tipo, String tipoSpecifico, String titolo, String[] testo, String[] soluzione) {
 		Enigma enigma = new Enigma(autore, tipo, tipoSpecifico, titolo, testo, soluzione); 
-		enigma = enigmiRepository.save(enigma);
-		return enigma;
+		return enigmiRepository.save(enigma);
 	}
 
  	public Enigma getEnigma(Long id) {
-		Enigma enigma = enigmiRepository.findById(id).orElse(null);
-		return enigma;
+		return enigmiRepository.findById(id).orElse(null);
 	}
 
 	public Collection<Enigma> getEnigmi() {
-		Collection<Enigma> enigmi = enigmiRepository.findAll();
-		return enigmi;
+		return enigmiRepository.findAll();
 	}
 
 	public Collection<Enigma> getEnigmiByAutore(String autore) {
-		Collection<Enigma> enigmi = enigmiRepository.findByAutore(autore);
-		return enigmi;
+		return enigmiRepository.findByAutore(autore);
 	}
 
 	public Collection<Enigma> getEnigmiByAutori(Collection<String> autori) {
-		Collection<Enigma> enigmi = enigmiRepository.findByAutoreIn(autori);
-		return enigmi;
+		return enigmiRepository.findByAutoreIn(autori);
 	}
 
 	public Collection<Enigma> getEnigmiByTipo(String tipo) {
-		Collection<Enigma> enigmi = enigmiRepository.findByTipo(tipo);
-		return enigmi;
+		return enigmiRepository.findByTipo(tipo);
 	}
 
 	public Collection<Enigma> getEnigmiByTipi(Collection<String> tipi) {
-		Collection<Enigma> enigmi = enigmiRepository.findByTipoIn(tipi);
-		return enigmi;
+		return enigmiRepository.findByTipoIn(tipi);
 	}
-
 }
