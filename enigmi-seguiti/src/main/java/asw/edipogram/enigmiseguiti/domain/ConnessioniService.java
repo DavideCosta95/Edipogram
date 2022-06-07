8439458ab93d14d64a03f3d27d74d08b169f3571
Gren <dashish95@gmail.com>
2022-06-07 23:49:46 +0200

    feat(enigmi-seguiti): switch services for using local DB

--- enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/ConnessioniService.java
+++ enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/ConnessioniService.java
@@ -1,9 +1,33 @@
 package asw.edipogram.enigmiseguiti.domain;
 
-import java.util.*; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface ConnessioniService {
+import java.util.Collection;
 
-	Collection<Connessione> getConnessioniByUtente(String utente);
-	
+@Service
+public class ConnessioniService {
+
+	private final ConnessioniRepository connessioniRepository;
+
+	@Autowired
+	private ConnessioniService(ConnessioniRepository connessioniRepository) {
+		this.connessioniRepository = connessioniRepository;
+	}
+
+ 	public Connessione addConnessione(Connessione connessione) {
+		return connessioniRepository.save(connessione);
+	}
+
+ 	public Connessione getConnessione(Long id) {
+		return connessioniRepository.findById(id).orElse(null);
+	}
+
+ 	public Collection<Connessione> getConnessioni() {
+		return connessioniRepository.findAll();
+	}
+
+	public Collection<Connessione> getConnessioniByUtente(String utente) {
+		return connessioniRepository.findByUtente(utente);
+	}
 }
--- enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmiService.java
+++ enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/domain/EnigmiService.java
@@ -1,9 +1,45 @@
 package asw.edipogram.enigmiseguiti.domain;
 
-import java.util.*; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface EnigmiService {
+import java.util.Collection;
 
-	Collection<Enigma> getEnigmiByTipi(Collection<String> tipi);
+@Service
+public class EnigmiService {
 
+	private final EnigmiRepository enigmiRepository;
+
+	@Autowired
+	private EnigmiService(EnigmiRepository enigmiRepository) {
+		this.enigmiRepository = enigmiRepository;
+	}
+
+ 	public Enigma addEnigma(Enigma enigma) {
+		return enigmiRepository.save(enigma);
+	}
+
+ 	public Enigma getEnigma(Long id) {
+		return enigmiRepository.findById(id).orElse(null);
+	}
+
+	public Collection<Enigma> getEnigmi() {
+		return enigmiRepository.findAll();
+	}
+
+	public Collection<Enigma> getEnigmiByAutore(String autore) {
+		return enigmiRepository.findByAutore(autore);
+	}
+
+	public Collection<Enigma> getEnigmiByAutori(Collection<String> autori) {
+		return enigmiRepository.findByAutoreIn(autori);
+	}
+
+	public Collection<Enigma> getEnigmiByTipo(String tipo) {
+		return enigmiRepository.findByTipo(tipo);
+	}
+
+	public Collection<Enigma> getEnigmiByTipi(Collection<String> tipi) {
+		return enigmiRepository.findByTipoIn(tipi);
+	}
 }
