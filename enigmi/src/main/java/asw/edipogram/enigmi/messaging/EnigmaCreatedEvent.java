26ab40156712d3d4fae804b06e775c48c78de4ff
Gren <dashish95@gmail.com>
2022-06-07 23:54:32 +0200

    feat(enigmi): add kafka messaging domain classes

---
+++ enigmi/src/main/java/asw/edipogram/enigmi/messaging/EnigmaCreatedEvent.java
@@ -0,0 +1,29 @@
+package asw.edipogram.enigmi.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnigmaCreatedEvent {
+	@NonNull
+	private String autore;
+
+	@NonNull
+	private String tipo;
+
+	@NonNull
+	private String tipoSpecifico;
+
+	@NonNull
+	private String titolo;
+
+	@NonNull
+	private String[] testo;
+
+	@NonNull
+	private String[] soluzione;
+}
---
+++ enigmi/src/main/java/asw/edipogram/enigmi/utils/EnigmaModelConverter.java
@@ -0,0 +1,32 @@
+package asw.edipogram.enigmi.utils;
+
+import asw.edipogram.enigmi.domain.Enigma;
+import asw.edipogram.enigmi.messaging.EnigmaCreatedEvent;
+import asw.edipogram.enigmi.rest.CreateEnigmaRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EnigmaModelConverter {
+
+	private EnigmaModelConverter() {}
+
+	public Enigma toEnigma(CreateEnigmaRequest createEnigmaRequest) {
+		String autore = createEnigmaRequest.getAutore();
+		String tipo = createEnigmaRequest.getTipo();
+		String tipoSpecifico = createEnigmaRequest.getTipoSpecifico();
+		String titolo = createEnigmaRequest.getTitolo();
+		String[] testo = createEnigmaRequest.getTesto();
+		String[] soluzione = createEnigmaRequest.getSoluzione();
+		return new Enigma(autore, tipo, tipoSpecifico, titolo, testo, soluzione);
+	}
+
+	public EnigmaCreatedEvent toEnigmaCreatedEvent(Enigma enigma) {
+		String autore = enigma.getAutore();
+		String tipo = enigma.getTipo();
+		String tipoSpecifico = enigma.getTipoSpecifico();
+		String titolo = enigma.getTitolo();
+		String[] testo = enigma.getTesto();
+		String[] soluzione = enigma.getSoluzione();
+		return new EnigmaCreatedEvent(autore, tipo, tipoSpecifico, titolo, testo, soluzione);
+	}
+}
