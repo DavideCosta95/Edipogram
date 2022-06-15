7bc122cb1f4f321aba6b2598c2ec47e45b966b40
Gren <dashish95@gmail.com>
2022-06-15 17:23:44 +0200

    refactor(enigmi-seguiti): move util class logic and introduce message consuming strategy map

--- enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/messaging/MessageConsumerFactory.java
+++ enigmi-seguiti/src/main/java/asw/edipogram/enigmiseguiti/messaging/MessageConsumerFactory.java
@@ -1,15 +1,13 @@
 package asw.edipogram.enigmiseguiti.messaging;
 
-import asw.edipogram.enigmiseguiti.domain.Connessione;
-import asw.edipogram.enigmiseguiti.domain.ConnessioniService;
-import asw.edipogram.enigmiseguiti.domain.Enigma;
-import asw.edipogram.enigmiseguiti.domain.EnigmiService;
-import asw.edipogram.enigmiseguiti.utils.EnigmaModelConverter;
+import asw.edipogram.enigmiseguiti.domain.*;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 @Slf4j
@@ -17,19 +15,20 @@ public class MessageConsumerFactory {
 
 	private final ConnessioniService connessioniService;
 	private final EnigmiService enigmiService;
-	private final EnigmaModelConverter enigmaModelConverter;
+
+	private final Map<String, MessageConsumer> topicName2messageConsumeStrategy;
 
 	@Autowired
-	private MessageConsumerFactory(ConnessioniService connessioniService, EnigmiService enigmiService, EnigmaModelConverter enigmaModelConverter) {
+	private MessageConsumerFactory(ConnessioniService connessioniService, EnigmiService enigmiService) {
 		this.connessioniService = connessioniService;
 		this.enigmiService = enigmiService;
-		this.enigmaModelConverter = enigmaModelConverter;
+		this.topicName2messageConsumeStrategy = getMessageConsumersMap();
 	}
 
-	public MessageConsumer getMessageConsumerByTopic(String topic) {
-		switch (topic) {
-			case "enigmi":
-				return message -> {
+	private Map<String, MessageConsumer> getMessageConsumersMap() {
+		return Map.of(
+				"enigmi",
+				message -> {
 					ObjectMapper objectMapper = new ObjectMapper();
 					EnigmaCreatedEvent enigmaCreatedEvent;
 					try {
@@ -38,12 +37,11 @@ public class MessageConsumerFactory {
 						log.error("Error while parsing as Enigma string: {}", message);
 						return;
 					}
-					Enigma enigma = enigmaModelConverter.toEnigma(enigmaCreatedEvent);
-					enigma = enigmiService.addEnigma(enigma);
+					Enigma enigma = enigmiService.addEnigma(toEnigma(enigmaCreatedEvent));
 					log.info("Add enigma: {}", enigma);
-				};
-			case "connessioni":
-				return message -> {
+				},
+				"connessioni",
+				message -> {
 					ObjectMapper objectMapper = new ObjectMapper();
 					ConnessioneCreatedEvent connessioneCreatedEvent;
 					try {
@@ -55,10 +53,21 @@ public class MessageConsumerFactory {
 					Connessione connessione = new Connessione(connessioneCreatedEvent.getUtente(), connessioneCreatedEvent.getTipo());
 					connessione = connessioniService.addConnessione(connessione);
 					log.info("Add connessione: {}", connessione);
-				};
-			default:
-				log.warn("Message type not recognized");
-				return message -> {};
-		}
+				}
+		);
+	}
+
+	public MessageConsumer getMessageConsumerByTopic(String topic) {
+		return topicName2messageConsumeStrategy.getOrDefault(topic, message -> log.warn("Message type not recognized"));
+	}
+
+	public Enigma toEnigma(EnigmaCreatedEvent enigmaCreatedEvent) {
+		String autore = enigmaCreatedEvent.getAutore();
+		String tipo = enigmaCreatedEvent.getTipo();
+		String tipoSpecifico = enigmaCreatedEvent.getTipoSpecifico();
+		String titolo = enigmaCreatedEvent.getTitolo();
+		String[] testo = enigmaCreatedEvent.getTesto();
+		String[] soluzione = enigmaCreatedEvent.getSoluzione();
+		return new Enigma(autore, tipo, tipoSpecifico, titolo, testo, soluzione);
 	}
 }
